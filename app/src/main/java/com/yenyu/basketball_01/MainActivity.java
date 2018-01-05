fc61ed99d94314106105bf593d1313d9774a4b66
yen <jinyen0531@gmail.com>
2018-01-05 15:06:33 +0800

    Initial commit

---
+++ app/src/main/java/com/yenyu/basketball_01/MainActivity.java
@@ -0,0 +1,23 @@
+package com.yenyu.basketball_01;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        Intent it= new Intent();
+        it.setClass(MainActivity.this, ActivityCheck.class);
+        startActivity(it);
+    }
+}
