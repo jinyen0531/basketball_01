45c3227edca7063947abae4ce5c4e873ce9df72b
yen <jinyen0531@gmail.com>
2018-01-16 16:33:04 +0800

    2018/01/16 findViewByID

--- app/src/main/java/com/yenyu/basketball_01/ActivityCheck.java
+++ app/src/main/java/com/yenyu/basketball_01/ActivityCheck.java
@@ -1,16 +1,77 @@
 package com.yenyu.basketball_01;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 
 public class ActivityCheck extends AppCompatActivity {
 
+    EditText edNum1,edNum2,edNum3,edNum4,edNum5,edNum6,edNum7,edNum8,edNum9,edNum10,edNum11,edNum12;
+    int i;
+    boolean check[] = new boolean[12];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check);
+        edNum1 =(EditText)findViewById(R.id.edNum1);
+        edNum2 =(EditText)findViewById(R.id.edNum2);
+        edNum3 =(EditText)findViewById(R.id.edNum3);
+        edNum4 =(EditText)findViewById(R.id.edNum4);
+        edNum5 =(EditText)findViewById(R.id.edNum5);
+        edNum6 =(EditText)findViewById(R.id.edNum6);
+        edNum7 =(EditText)findViewById(R.id.edNum7);
+        edNum8 =(EditText)findViewById(R.id.edNum8);
+        edNum9=(EditText)findViewById(R.id.edNum9);
+        edNum10 =(EditText)findViewById(R.id.edNum10);
+        edNum11 =(EditText)findViewById(R.id.edNum11);
+        edNum12 =(EditText)findViewById(R.id.edNum12);
+
+    }
+
+    public void clickOK(View v)
+    {
+        Intent it = new Intent(ActivityCheck.this,ButtonRecord.class);
+        it.putExtra("player1",edNum1.getText().toString());
+        it.putExtra("player2",edNum2.getText().toString());
+        it.putExtra("player3",edNum3.getText().toString());
+        it.putExtra("player4",edNum4.getText().toString());
+        it.putExtra("player5",edNum5.getText().toString());
+        it.putExtra("player6",edNum6.getText().toString());
+        it.putExtra("player7",edNum7.getText().toString());
+        it.putExtra("player8",edNum8.getText().toString());
+        it.putExtra("player9",edNum9.getText().toString());
+        it.putExtra("player10",edNum10.getText().toString());
+        it.putExtra("player11",edNum11.getText().toString());
+        it.putExtra("player12",edNum12.getText().toString());
+        startActivity(it);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ActivityCheck.this);
+        final String [] players = {edNum1.getText().toString(),
+                edNum2.getText().toString(),
+                edNum3.getText().toString(),
+                edNum4.getText().toString(),
+                edNum5.getText().toString(),
+                edNum6.getText().toString(),
+                edNum7.getText().toString(),
+                edNum8.getText().toString(),
+                edNum9.getText().toString(),
+                edNum10.getText().toString(),
+                edNum11.getText().toString(),
+                edNum12.getText().toString(),};
+        builder.setTitle("請選擇先發五人");
+        builder.setMultiChoiceItems(players, check, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+            }
+        });
+
+
+
     }
 
 
--- app/src/main/java/com/yenyu/basketball_01/ButtonRecord.java
+++ app/src/main/java/com/yenyu/basketball_01/ButtonRecord.java
@@ -2,12 +2,167 @@ package com.yenyu.basketball_01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class ButtonRecord extends AppCompatActivity {
-
+    Button bt2in,bt2out,bt3in,bt3out,btFTin,btFTout,btOR,btDR,btST,btAS,btBS,btTO,btFoul,btNextSection;
+    ToggleButton tbNumber1,tbNumber2,tbNumber3,tbNumber4,tbNumber5,tbOP;
+    TextView tvScore1,tvScore2,tvSectionFoul1,tvSectionFoul2,tvSection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.button_record);
+
+        tvScore1=(TextView)findViewById(R.id.tvScore1);
+        tvScore2=(TextView)findViewById(R.id.tvScore2);
+        tvSectionFoul1=(TextView)findViewById(R.id.tvSectionFoul1);
+        tvSectionFoul2=(TextView)findViewById(R.id.tvSectionFoul2);
+        tvSection=(TextView)findViewById(R.id.tvSection);
+
+        bt2in = (Button) findViewById(R.id.bt2in);
+        bt2out = (Button) findViewById(R.id.bt2out);
+        bt3in = (Button) findViewById(R.id.bt3in);
+        bt3out = (Button) findViewById(R.id.bt3out);
+        btFTin = (Button) findViewById(R.id.btFTin);
+        btFTout = (Button) findViewById(R.id.btFTout);
+        btOR = (Button) findViewById(R.id.btOR);
+        btDR = (Button) findViewById(R.id.btDR);
+        btST = (Button) findViewById(R.id.btST);
+        btAS = (Button) findViewById(R.id.btAS);
+        btBS = (Button) findViewById(R.id.btBS);
+        btTO = (Button) findViewById(R.id.btTO);
+        btFoul = (Button) findViewById(R.id.btFoul);
+        btNextSection = (Button) findViewById(R.id.btNextSection);
+
+        tbNumber1 =(ToggleButton) findViewById(R.id.tbNumber1);
+        tbNumber2 =(ToggleButton) findViewById(R.id.tbNumber2);
+        tbNumber3 =(ToggleButton) findViewById(R.id.tbNumber3);
+        tbNumber4 =(ToggleButton) findViewById(R.id.tbNumber4);
+        tbNumber5 =(ToggleButton) findViewById(R.id.tbNumber5);
+        tbOP =(ToggleButton) findViewById(R.id.tbOP);
+
+        bt2in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvScore1.setText(Integer.valueOf(tvScore1.getText().toString())+2);
+                Toast.makeText(ButtonRecord.this,"兩分球進",Toast.LENGTH_SHORT).show();
+            }
+        });
+        bt2out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"兩分球不進",Toast.LENGTH_SHORT).show();
+            }
+        });
+        bt3in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"三分球進",Toast.LENGTH_SHORT).show();
+            }
+        });
+        bt3out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"三分球不進",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btFTin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"罰球進",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btFTout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"罰球不進",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btOR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"進攻籃板",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btDR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"防守籃板",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btST.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"抄截",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btAS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"助攻",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btBS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"阻攻",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btTO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"失誤",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btFoul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ButtonRecord.this,"犯規",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tbNumber1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(ButtonRecord.this,"1",Toast.LENGTH_SHORT).show();
+            }
+        });
+        tbNumber2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(ButtonRecord.this,"2",Toast.LENGTH_SHORT).show();
+            }
+        });
+        tbNumber3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(ButtonRecord.this,"3",Toast.LENGTH_SHORT).show();
+            }
+        });
+        tbNumber4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(ButtonRecord.this,"4",Toast.LENGTH_SHORT).show();
+            }
+        });
+        tbNumber5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(ButtonRecord.this,"5",Toast.LENGTH_SHORT).show();
+            }
+        });
+        tbOP.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(ButtonRecord.this,"對手",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
